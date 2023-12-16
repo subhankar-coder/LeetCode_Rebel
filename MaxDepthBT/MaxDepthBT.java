6625a66178e58c03facf9562a6f14e0e5d533548
Subhankar <sub.nitt@gmail.com>
2023-12-16 17:50:56 +0530

    Max Depth Binary Tree

---
+++ MaxDepthBT/MaxDepthBT.java
@@ -0,0 +1,26 @@
+package MaxDepthBT;
+
+public class MaxDepthBT{
+
+	public static void main(String ... args){
+		 if (root==null){
+         return 0;
+     }
+     return 1+Math.max(maxDepth(root.left),maxDepth(root.right));   
+    }
+
+	}
+}
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This is a repository for leetcode solution topic wise
+git hub token- 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
 
