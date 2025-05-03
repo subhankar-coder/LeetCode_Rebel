18c042d9f805cea40a8606269d250ea78416db70
subhankar <sub.nitt@gmail.com>
2025-05-03 22:05:28 +0530

    1007 min domino rotation for equal row https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/?envType=daily-question&envId=2025-05-03

---
+++ 1007_min_domino_rotation.go
@@ -0,0 +1,66 @@
+package main
+
+import(
+	"fmt"
+	// "math"
+)
+
+// type Pair struct{
+// 	X int
+// 	Y int
+// }
+func main(){
+	
+	tops := []int {2,1,2,4,2,2}
+	bottoms  := []int {5,2,6,2,3,2}
+	var res int =minDominoRotations(tops,bottoms)
+	
+    fmt.Println(res)
+}
+
+  func minDominoRotations(tops []int, bottoms []int) int {
+
+    var len int = len(tops)
+    // pairs := make([]Pair,len)
+
+	// for i:=0;i<len;i++{
+	// 	pairs[i][0]=tops[i]
+	// 	pairs[i][1]=bottoms[i]
+	// }
+
+	var topCandidate int = tops[0]
+	var bottomCandidate int = bottoms[0]
+
+	var topCount int = calculateMinRotation(tops,bottoms,topCandidate,len)
+	if topCount !=-1{
+		return topCount
+	}else if topCandidate != bottomCandidate{
+
+		return calculateMinRotation(tops,bottoms,bottomCandidate,len)
+	}
+	return -1
+	     
+}
+
+func calculateMinRotation(tops[]int,bottoms[]int,key int,len int) int{
+	var topConversion int =0
+	var bottomConversion int =0 
+	for i:=0;i<len;i++{
+		if key != bottoms[i] && key != tops[i]{
+			return -1
+		}else if key != tops[i]{
+			topConversion++
+		}else if key != bottoms[i]{
+			bottomConversion++
+		}
+	}
+	return min(topConversion, bottomConversion) 
+}
+
+func min(a,b int)int{
+		if a<b{
+			return a
+		}
+		return b
+}
+
