d578ca7b2bf49be12e74f12b0fdaa1323c31f498
Subhankar Biswas <sub.nitt@gmail.com>
2025-06-02 08:23:07 -0400

    https://leetcode.com/problems/candy/?envType=daily-question&envId=2025-06-02

---
+++ 135_candy_line.py
@@ -0,0 +1,15 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+      n = len(ratings)
+      result = [1]*n
+      for i in range(1,n,1):
+        result[i] = result[i-1]+1 if ratings[i-1]<ratings[i] else 1
+      for i in range(n-2,-1,-1):
+        result[i] = max(result[i],result[i+1]+1 if ratings[i+1]<ratings[i] else 1)
+        
+      return sum(result)
+
+if __name__ == "__main__":
+  ratings: List = [1,2,87,87,87,2,1]
+  s = Solution()
+  print(s.candy(ratings))
