4aa7d5b8cd8a580f4598390a4f906d435a7ea826
subhankarbiswas30000 <subhankarbiswas30000@gmail.com>
2025-06-09 19:21:20 +0000

    https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/description/?envType=daily-question&envId=2025-06-07

---
+++ 440_kth_lex.py
@@ -0,0 +1,33 @@
+
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        def count_ele_btwn(n,start,end):
+            count = 0
+            while start <= n:
+                count += min(end,n+1) - start
+                start *=10
+                end *=10
+            
+            return count
+        curr = 1
+        while k >0:
+            count = count_ele_btwn(n,curr,curr+1)
+            print(curr,count,k)
+            if count < k:
+                k -= count
+                curr+=1
+            else:
+                k-=1
+                curr = curr*10
+        
+        return curr
+        
+
+
+
+       
+
+if __name__ == "__main__":
+    s = Solution()
+    n,k = 100,10
+    print(s.findKthNumber(n,k))
