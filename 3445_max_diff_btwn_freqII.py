a593f02b235f59085ac53667c48994dc004a5455
subhankarbiswas30000 <subhankarbiswas30000@gmail.com>
2025-06-12 16:12:30 +0000

    https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/description/?envType=daily-question&envId=2025-06-12

---
+++ 3445_max_diff_btwn_freqII.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxDifference(self, s: str, k: int) -> int:
+        
+
+
+
+
+
+
+if __name__ == "__main__":
+    s,k = "12233", 4
+    s = Solution()
+    print(s.maxDifference(s,k))
